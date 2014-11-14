6a3894845bdaac5a9e14f5d204e7d4373bc94be8
Shawn McCool <shawn@heybigname.com>
2014-11-14 16:24:14 +0100

    slap in some pointless examples

--- MyApp/Ui/Web/Controllers/ExampleController.php
+++ MyApp/Ui/Web/Controllers/ExampleController.php
@@ -2,4 +2,7 @@
 
 final class ExampleController {
 
+    public function examplePage() {
+        return "I am putting myself to the fullest possible use, which is all I think that any conscious entity can ever hope to do.";
+    }
 }
